0fcf1367ef82f30f508be7730603fc1b8d94e04a
Michel GILBERT <michel.1.gilbert@arval.fr>
2023-08-24 17:41:26 +0200

    add login page

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,7 +1,21 @@
-import React from "react";
+import { Link } from "react-router-dom";
 
 const Login = () => {
-  return <div>Login</div>;
+  return (
+    <div className="auth">
+      <h1>Login</h1>
+      <form action="">
+        <input required type="text" placeholder="username" />
+        <input required type="password" placeholder="password" />
+        <button>Login</button>
+        <p>This is an error !</p>
+        <span>
+          Don&apos;t you have an account ?{" "}
+          <Link to="/register">Register</Link>{" "}
+        </span>
+      </form>
+    </div>
+  );
 };
 
 export default Login;
