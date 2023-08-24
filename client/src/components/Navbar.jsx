9a8fc1850ba5d39392fb37284146f734eb473c02
Michel GILBERT <michel.1.gilbert@arval.fr>
2023-08-24 17:41:50 +0200

    add navbar components

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,46 @@
+import Logo from "../assets/img/treblig-blog.png";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+  return (
+    <div className="navbar">
+      <div className="container">
+        <div className="logo">
+          <img src={Logo} alt="logo" />
+        </div>
+        <div className="links">
+          <Link className="link" to="/?cat=art">
+            <h6>ART</h6>
+          </Link>
+          <Link className="link" to="/?cat=science">
+            <h6>SCIENCE</h6>
+          </Link>
+          <Link className="link" to="/?cat=technology">
+            <h6>TECHNOLOGY</h6>
+          </Link>
+          <Link className="link" to="/?cat=cinema">
+            <h6>CINEMA</h6>
+          </Link>
+          <Link className="link" to="/?cat=design">
+            <h6>DESIGN</h6>
+          </Link>
+          <Link className="link" to="/?cat=food">
+            <h6>FOOD</h6>
+          </Link>
+          <Link className="link" to="/?cat=photography">
+            <h6>PHOTOGRAPHY</h6>
+          </Link>
+          <span>Michel</span>
+          <span>Logout</span>
+          <span className="write">
+            <Link className="link" to="/write">
+              Write
+            </Link>
+          </span>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Navbar;
